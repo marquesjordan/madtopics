8b666c2b6bb031b1f8a61f2761362dcc08edf9fa
Marques Jordan <marques.jordan81@gmail.com>
2014-10-24 15:45:43 -0700

    Add css and bootstrap components

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  get 'pages/home'
+  root 'pages#home'
 
   get 'questions/' => 'questions#index', as: :questions
   get 'questions/new' => 'questions#new', as: :new_question
